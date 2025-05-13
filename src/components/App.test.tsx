0337a3d8c35f22c014438667f864de2906f75d1e
liyunze <50455574+liyunze-coding@users.noreply.github.com>
2025-05-14 09:36:33 +1000

    add more tests

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -6,3 +6,18 @@ test('renders the home page', () => {
     render(<Home />);
     expect(screen.getByText(/HexGuess/i)).toBeInTheDocument();
 });
+
+test('renders the "Rules" button', () => {
+    render(<Home />);
+    expect(screen.getByRole('button', { name: /RULES/i })).toBeInTheDocument();
+});
+
+test('renders the "What is Hex?" button', () => {
+    render(<Home />);
+    expect(screen.getByRole('button', { name: /WHAT IS HEX\?/i })).toBeInTheDocument();
+});
+
+test('renders the "Credits" button', () => {
+    render(<Home />);
+    expect(screen.getByRole('button', { name: /CREDITS/i })).toBeInTheDocument();
+});
